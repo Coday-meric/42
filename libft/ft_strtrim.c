ae467e5343ec7fbce63666f660d788968e778165
amaisonn <amaisonn@student.42.fr>
2023-02-10 15:39:24 +0100

    strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,48 @@
 /*   By: amaisonn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 00:18:32 by amaisonn          #+#    #+#             */
-/*   Updated: 2023/02/09 00:24:29 by amaisonn         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:37:44 by amaisonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	ft_index(char const *s1, char const *set, size_t sens)
+{
+	size_t     i;
+        int     res;
+
+        i = sens;
+        res = 0;
+        while (s1[i])
+        {
+                while (*set)
+                {
+                        if (s1[i] == *set)
+                                res = 1;
+			set++;
+                }
+                if (res != 1)
+                        return (i);
+		if (sens == 0)
+			i++;
+		else
+			i--;
+        }
+
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	Id;
+	size_t	If;
+	char	*res;
 	
+	Id = ft_index(s1, set, 0);
+	If = ft_index(s1, set, ft_strlen(s1));
+	res = malloc((ft_strlen(s1) - Id) * sizeof(char));
+	if (!res)
+		return (NULL);
+	res = ft_substr(s1, Id, (ft_strlen(s1) - Id));
+	return (res);
 }
